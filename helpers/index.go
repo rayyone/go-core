77f3ecb47de8dabd3b6916c3d0dbcfed1f700eb6
Roy Nguyen <roy.nguyen@rayyone.com>
2022-10-06 14:45:11 +0700

    Add import for helpers

---
+++ helpers/index.go
@@ -0,0 +1,16 @@
+package helpers
+
+import _ "github.com/rayyone/go-core/helpers/array"
+import _ "github.com/rayyone/go-core/helpers/dataparser"
+import _ "github.com/rayyone/go-core/helpers/file"
+import _ "github.com/rayyone/go-core/helpers/httpclient"
+import _ "github.com/rayyone/go-core/helpers/image"
+import _ "github.com/rayyone/go-core/helpers/istype"
+import _ "github.com/rayyone/go-core/helpers/log"
+import _ "github.com/rayyone/go-core/helpers/maps"
+import _ "github.com/rayyone/go-core/helpers/method"
+import _ "github.com/rayyone/go-core/helpers/net"
+import _ "github.com/rayyone/go-core/helpers/pagination"
+import _ "github.com/rayyone/go-core/helpers/response"
+import _ "github.com/rayyone/go-core/helpers/retry"
+import _ "github.com/rayyone/go-core/helpers/str"
