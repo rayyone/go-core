949b9396df52842a0ba40a01e896d52b20d1d37a
thinh.nguyen <thinh.nguyen@rayyone.com>
2022-12-09 17:18:04 +0700

    update

--- ryerr/err.go
+++ ryerr/err.go
@@ -148,15 +148,15 @@ func (c Err) Report() {
 	slackMsg := fmt.Sprintf("*%s*\n", c.Error())
 	if eventId != nil {
 		slackMsg += fmt.Sprintf("*EventID:* %s\n", *eventId)
-		if sentryProjectUrl := ry_slack.CurrentSlackClient().GetOption("sentry_project_url"); sentryProjectUrl != nil {
-			slackMsg += fmt.Sprintf("<%s?query=%s|See more detail>", sentryProjectUrl, *eventId)
-		}
+		//if sentryProjectUrl := ry_slack.CurrentSlackClient().GetOption("sentry_project_url"); sentryProjectUrl != nil {
+		//	slackMsg += fmt.Sprintf("<%s?query=%s|See more detail>", sentryProjectUrl, *eventId)
+		//}
 	}
-	if errChannel := ry_slack.CurrentSlackClient().GetOption("error_channel"); errChannel != nil {
-		ry_slack.SendSimpleMessageToChannel(errChannel.(string), "ry-api error", slackMsg)
-	} else {
+	//if errChannel := ry_slack.CurrentSlackClient().GetOption("error_channel"); errChannel != nil {
+	//	ry_slack.SendSimpleMessageToChannel(errChannel.(string), "ry-api error", slackMsg)
+	//} else {
 		ry_slack.SendSimpleMessage("ry-api error", slackMsg)
-	}
+	//}
 }
 
 // New creates a no type error and report to sentry
